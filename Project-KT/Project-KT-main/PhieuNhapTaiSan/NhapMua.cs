2121ffc159c5a4d9f559a163f250f8d44638300f
Hoàng Phương Thể <62139118+HoangPhuongThe@users.noreply.github.com>
2021-01-04 14:44:17 +0700

    nguyendu
    nguyendubudit

---
+++ Project-KT/Project-KT-main/PhieuNhapTaiSan/NhapMua.cs
@@ -0,0 +1,230 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace PhieuNhapTaiSan
+{
+    public partial class NhapMua : Form
+    {
+        SqlConnection con = new SqlConnection(@"Data Source=LAPTOP-MCG6S6JA\SQLEXPRESS;Initial Catalog=ketoan;Integrated Security=True;");       
+        SqlCommand cmd;
+        SqlDataAdapter adpt;
+        DataSet ds;
+        DataTable dt;
+        int ID = 0;
+        public NhapMua()
+        {
+            InitializeComponent();
+        }
+        private void ketnoicsdl()
+        {
+            con.Open();
+            string sql = "select * from ts";  
+            SqlCommand com = new SqlCommand(sql, con); //bat dau truy van
+            com.CommandType = CommandType.Text;
+            SqlDataAdapter da = new SqlDataAdapter(com); //chuyen du lieu ve
+            DataTable dt = new DataTable(); //tạo một kho ảo để lưu trữ dữ liệu
+            da.Fill(dt);  // đổ dữ liệu vào kho
+            con.Close();  // đóng kết nối
+            dataGridView1.DataSource = dt; //đổ dữ liệu vào datagridview
+        }
+        private void LoadTKduDBCombox()
+        {
+            con.Open();
+            string sql = "select MaTK from DMTK";
+            SqlCommand com = new SqlCommand(sql, con); //bat dau truy van
+            com.CommandType = CommandType.Text;
+            SqlDataAdapter da = new SqlDataAdapter(com); //chuyen du lieu ve
+            DataSet ds = new DataSet(); //tạo một kho ảo để lưu trữ dữ liệu
+            da.Fill(ds);  // đổ dữ liệu vào kho
+            cbTkdu.DataSource = ds.Tables[0];
+            cbTkdu.DisplayMember = "MaTK";
+            con.Close();  // đóng kết nối
+
+        }
+        
+        private void LoadTKhoanDBCombox()
+        {
+            con.Open();
+            string sql = "select MaTK,TenMaTK from DMTK";
+            SqlCommand com = new SqlCommand(sql, con); //bat dau truy van
+            com.CommandType = CommandType.Text;
+            SqlDataAdapter da = new SqlDataAdapter(com); //chuyen du lieu ve
+            DataSet ds = new DataSet(); //tạo một kho ảo để lưu trữ dữ liệu
+            da.Fill(ds);  // đổ dữ liệu vào kho
+            cbTK.DataSource = ds.Tables[0];
+            cbTK.DisplayMember = "MaTK";
+            cbTK.ValueMember = "TenMaTK";
+            tbTenTaiKhoan.DataBindings.Add("Text", cbTK.DataSource,"TenMaTK");
+            tbDes1.DataBindings.Add("Text", cbTK.DataSource, "TenMaTK");
+            con.Close();  // đóng kết nối
+
+        }
+
+        private void LoadKHangDBCombox()
+        {
+            con.Open();
+            string sql = "select MSKH,TENKH, dchi from DMKH";
+            SqlCommand com = new SqlCommand(sql, con); //bat dau truy van
+            com.CommandType = CommandType.Text;
+            SqlDataAdapter da = new SqlDataAdapter(com); //chuyen du lieu ve
+            DataSet ds = new DataSet(); //tạo một kho ảo để lưu trữ dữ liệu
+            da.Fill(ds);  // đổ dữ liệu vào kho
+            comboMKH.DataSource = ds.Tables[0];
+            comboMKH.DisplayMember = "MSKH";
+            comboMKH.ValueMember = "TENKH";
+            comboMKH.ValueMember = "dchi";
+            tbTenKH.DataBindings.Add("Text", comboMKH.DataSource, "TENKH");
+            tbDiaChi.DataBindings.Add("Text", comboMKH.DataSource, "dchi");
+            con.Close();  // đóng kết nối
+
+        }
+
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lbMauSo_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            if (tbPso.Text != ""  & cbTkdu.Text != "" & tbGia.Text != "")
+            {
+                con.Open();
+                /*string sql = string.Format("INSERT INTO[KT].[dbo].[TS](tkdu, MSTS, st, pso) VALUES", tbPso.Text,tbMSTS.Text,cbTkdu.Text,tbGia.Text);
+
+                SqlCommand cmd = new SqlCommand(sql, con);*/
+
+                SqlCommand cmd = new SqlCommand("INSERT INTO [ketoan].[dbo].[TS](tkdu,st,sl,nsudung,snkh,pso) VALUES " +
+                    "    ('" + cbTkdu.Text + "','" + tbGia.Text + "','" + numSL.Text + "','" + dtpkNSuDung.Text + "','" + tbSoNKH.Text + "','" + tbPso.Text + "');", con);
+                cmd.ExecuteNonQuery();
+
+
+                /*SqlCommand cmd2 = new SqlCommand("INSERT INTO [KT].[dbo].[PH](pso,MSKH,ten,ldo,diachi) VALUES " +
+                    "    ('" + tbPso.Text + "','" + tbDonVi.Text + "','" + tbLyDo.Text + "','" + tbDiaChi.Text  + "');", con);
+
+                cmd2.ExecuteNonQuery();*/
+
+                con.Close();
+                MessageBox.Show("Dữ liệu đã được thêm vào ", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+        public DataTable XemDL(string sql)
+        {
+
+
+            SqlDataAdapter adap = new SqlDataAdapter(sql, con);
+            DataTable dt = new DataTable();
+            adap.Fill(dt);
+
+            return dt;
+        }
+        private void NhapMua_Load(object sender, EventArgs e)
+        {
+            ketnoicsdl();
+            XemDL("select * from ts ");
+            this.LoadTKduDBCombox();
+            this.LoadTKhoanDBCombox();
+            this.LoadKHangDBCombox();
+        }
+
+        private void tbNgayLap_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cbTK_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void tbTenTaiKhoan_TextChanged(object sender, EventArgs e)
+        {
+           
+
+        }
+
+        private void tbMaKhach_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Form_Main m = new Form_Main();
+            m.ShowDialog();
+            this.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            float sotien = Convert.ToInt32(tbGia.Text);
+            float thue = Convert.ToInt32(tbVAT.Text);
+
+            float kq = Convert.ToInt32(sotien * (thue / 100));
+            float trigia = Convert.ToInt32(sotien + kq);
+            tbTriGia.Text = trigia.ToString();
+            tbThue.Text = kq.ToString();
+            tbTienHang.Text = sotien.ToString();
+            tbCong.Text = sotien.ToString();
+        }
+
+        private void tbVAT_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tbLyDo_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void cbTkdu_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tbDes1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCheckpso_Click(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = XemDL("select * from ts where pso like '%" + tbPso.Text.Trim() + "%' ");
+        }
+    }
+}
---
+++ Project-KT/Project-KT-main/PhieuNhapTaiSan/khauhao.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace PhieuNhapTaiSan
+{
+    partial class khauhao
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.Đóng = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.tkdu = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.MSTS = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.st = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.nsdung = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(403, 66);
+            this.dateTimePicker1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(265, 22);
+            this.dateTimePicker1.TabIndex = 0;
+            this.dateTimePicker1.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(272, 66);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Tháng tính";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(591, 311);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(155, 28);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Tổng khấu hao";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(799, 311);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(132, 22);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Đóng
+            // 
+            this.Đóng.Location = new System.Drawing.Point(252, 311);
+            this.Đóng.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Đóng.Name = "Đóng";
+            this.Đóng.Size = new System.Drawing.Size(100, 28);
+            this.Đóng.TabIndex = 6;
+            this.Đóng.Text = "Đóng";
+            this.Đóng.UseVisualStyleBackColor = true;
+            this.Đóng.Click += new System.EventHandler(this.Đóng_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.label2.Location = new System.Drawing.Point(351, 11);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(361, 46);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Khấu Hao Tài Sản";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.tkdu,
+            this.MSTS,
+            this.st,
+            this.nsdung});
+            this.dataGridView1.Location = new System.Drawing.Point(100, 98);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.Size = new System.Drawing.Size(875, 185);
+            this.dataGridView1.TabIndex = 2;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // tkdu
+            // 
+            this.tkdu.HeaderText = "TKDU";
+            this.tkdu.MinimumWidth = 6;
+            this.tkdu.Name = "tkdu";
+            this.tkdu.ReadOnly = true;
+            this.tkdu.Width = 125;
+            // 
+            // MSTS
+            // 
+            this.MSTS.HeaderText = "MSTS";
+            this.MSTS.MinimumWidth = 6;
+            this.MSTS.Name = "MSTS";
+            this.MSTS.ReadOnly = true;
+            this.MSTS.Width = 125;
+            // 
+            // st
+            // 
+            this.st.HeaderText = "Nguyên Giá";
+            this.st.MinimumWidth = 6;
+            this.st.Name = "st";
+            this.st.ReadOnly = true;
+            this.st.Width = 125;
+            // 
+            // nsdung
+            // 
+            this.nsdung.HeaderText = "Ngày SD";
+            this.nsdung.MinimumWidth = 6;
+            this.nsdung.Name = "nsdung";
+            this.nsdung.Width = 125;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(100, 311);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 28);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Lưu và in";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // khauhao
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Đóng);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "khauhao";
+            this.Text = "khauhao";
+            this.Load += new System.EventHandler(this.khauhao_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button Đóng;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn tkdu;
+        private System.Windows.Forms.DataGridViewTextBoxColumn MSTS;
+        private System.Windows.Forms.DataGridViewTextBoxColumn st;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nsdung;
+        private System.Windows.Forms.Button button2;
+    }
+}
